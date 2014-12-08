2c82873815e3718a85b4f35958982bc169f28b1e
Kosinkadink <kosinkadink1@gmail.com>
2014-12-07 22:29:25 -0600

    Fixed spacing issues, changed how text scrolls

--- techtem_chatclient.py
+++ techtem_chatclient.py
@@ -18,14 +18,14 @@ def chat_client():
 		print 'Unable to connect'
 		sys.exit()
 	
-	print 'Connected to remote host. You can start sending messages'
+	print 'Connected to remote host.'
 	
 	#request information
-	name = raw_input("Name (optional):")
+	name = raw_input("Name (optional): ")
 	if name == "":
 		tripcode = ""
 	else:
-		tripcode = raw_input("Tripcode (also optional):")
+		tripcode = raw_input("Tripcode (also optional): ")
 
 	sys.stdout.write("\n[" + name + "] "); sys.stdout.flush()
  
@@ -44,8 +44,11 @@ def chat_client():
 					sys.exit()
 				else :
 					#print data
-					sys.stdout.write(data)
-					sys.stdout.write('\n[' + name + '] '); sys.stdout.flush()	 
+					sys.stdout.write("\r") # brings cursor to the beginning of the line, probably skips line
+					sys.stdout.write("\033[F") # brings cursor up one line, where the old text is now
+					sys.stdout.write('\n' + data) # skips line (due to change with server), writes data on top of old text
+					sys.stdout.write('\n[' + name + '] ') # skips to new first line, rewrites name
+					sys.stdout.flush()	 
 			
 			else :
 				# user entered a message
@@ -59,13 +62,14 @@ def chat_client():
 					elif message.split()[0] == "/quit" or message.split()[0] == "/leave":
 						quit()
 					elif message.split()[0] == "/help" or message.split()[0] == "/?":
-						sys.stdout.write("\nThanks for using the techtemchat client. Here are the commands you currently have available:\n/changename: changes your name\n/changetripcode: changes your trip code.\n/quit OR /leave: exits gracefully\n/help OR /?: Displays this menu.")
+						sys.stdout.write("\nThanks for using the techtemchat client. Here are the commands you currently have available:\n/changename + new name: changes your name\n/changetripcode + new tripcode: changes your trip code.\n/quit OR /leave: exits gracefully\n/help OR /?: Displays this menu.\n")
 					else:
 						print "Invalid command"
 				else:
 					data = message + name + "\n" + tripcode
 					s.send(data)
-				sys.stdout.write('\n[' + name + '] '); sys.stdout.flush() 
+				sys.stdout.write('[' + name + '] ')
+				sys.stdout.flush() 
 
 if __name__ == "__main__":
 
